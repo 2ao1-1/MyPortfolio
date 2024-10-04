50ef73e62b99907144f627afed31dad8d67e648b
2ao1 <ahmedomran1950@gmail.com>
2024-10-04 17:06:22 +0300

    responsive u.2

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const menuBtn = document.querySelector(".links");
+// const menuShow = document.querySelector(".links--list");
+
+// menuBtn.addEventListener("click", function () {
+//   menuShow.classList.toggle("hidden");
+// });
+
+// const darkBtn = document.querySelector(".dark--light");
+// const main = document.getElementsByTagName("main")[0];
+// darkBtn.addEventListener("click", function () {
+//   main.style.background = "#202020";
+//   main.style.color = "#fff";
+// });
